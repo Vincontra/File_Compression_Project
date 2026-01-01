e28ac3f80cd0ace55eb9c8d51f30510f9159f355
vinay <vinaygadge2003@gmail.com>
2026-01-01 22:58:39 +0530

    add Node data structure

--- src/src/Node.java
+++ src/src/Node.java
@@ -1,20 +1,20 @@
 public class Node implements Comparable<Node>{
-    Node left;
+    Node lefT;
     Node right;
     char character;
     int frequency;
-    Node(final char character,final int frequency,final Node left,final Node right) throws IllegalAccessException {
+    Node(final char character, final int frequency, final Node lefT, final Node right) throws IllegalAccessException {
         this.character=character;
         if (frequency<0){
             throw new IllegalArgumentException("Error:Frequency must be >=0");
         }
         this.frequency=frequency;
-        this.left=left;
+        this.lefT = lefT;
         this.right=right;
     }
 
     public boolean isLeaf(){
-        return this.left==null&&this.right==null;
+        return this.lefT ==null&&this.right==null;
     }
     public int compareTo(final Node that){
         final int fre=Integer.compare(this.frequency,that.frequency);
